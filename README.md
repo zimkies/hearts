82fd7edaa7147c08f056ae000df8a169619fd22f
Benjamin Kies <ben@joingrouper.com>
2020-11-15 19:06:18 -0500

    Add game fetcher

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # hearts
+
+# To run:
+
+
+https://developer.okta.com/blog/2018/12/20/crud-app-with-python-flask-react
+
+1. Install pipenv
+
+2. Install python libraries:
+
+pipenv install
+
+3. Run the flask app
+
+pipenv run flask run
--- app/routes.py
+++ app/routes.py
@@ -9,9 +9,19 @@ def index():
     return render_template('base.html')
 
 
-Game = namedtuple('Game', ["id"])
+Game = namedtuple('Game', ["id", "state", "players"])
 
 @app.route('/api/games/', methods=['post'])
 def create_game():
     game = Game(id='chicken')
     return jsonify(game._asdict())
+
+@app.route('/api/games/:id', methods=['get'])
+def fetch_game(id):
+    game = GameRepository.get(id)
+    return jsonify(game._asdict())
+
+class GameRepository():
+    @staticmethod
+    def get(id):
+        return Game(id='chicken')
